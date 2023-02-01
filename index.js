43c1de4cdcb3ce1af703352b821f6366ade2fd1e
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-01 19:12:56 +0200

    Refactor

--- index.js
+++ index.js
@@ -3,24 +3,39 @@ const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
 
+const HTTP_OK = 200;
+const HTTP_NOT_FOUND = 404;
+const PORT_NUMBER = 8000;
+const HOSTNAME = 'localhost';
+
+
 // top level code execute once
 const data = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'));
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const templateProductAttributes = {
+    "id": /{%ID%}/g,
+    "productName": /{%PRODUCTNAME%}/g,
+    "image": /{%IMAGE%}/g,
+    "from": /{%FROM%}/g,
+    "nutrients": /{%NUTRIENTS%}/g,
+    "quantity": /{%QUANTITY%}/g,
+    "price": /{%PRICE%}/g,
+    "description": /{%DESCRIPTION%}/g
+};
+
 
 const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    let output = temp;
+    for (const attribute in templateProductAttributes) {
+        output = output.replace(templateProductAttributes[attribute], product[attribute])
+    }
+
     if (!product.organic) {
         output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     }
+
     return output;
 }
 
@@ -42,20 +57,20 @@ const server = http.createServer((request, response) => {
         //__dirname: where the current file is located
         // .: where the current file executed
 
-        response.writeHead(200, {
+        response.writeHead(HTTP_OK, {
             'Content-type': 'application/json'
         });
 
         response.end(JSON.stringify(data));
 
     } else {
-        response.writeHead(404, {
+        response.writeHead(HTTP_NOT_FOUND, {
             'Content-type': 'text/html'
         });
         response.end('<h1>Page not found!</h1>');
     }
 });
 
-server.listen(8000, 'localhost', () => {
-    console.log('Listening to requests on port 8000');
+server.listen(PORT_NUMBER, HOSTNAME, () => {
+    console.log(`Listening on ${HOSTNAME} to requests on port ${PORT_NUMBER}`);
 });
